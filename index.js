08cd348ca865cd28e1f2e98fde0f669f9c8f424e
Evan  Markert <evanmarkert@gmail.com>
2019-08-04 16:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here:
+
+
+function driversWithRevenueOver(arr, value) {
+  return arr.filter(function(e) {
+     return e.revenue > value
+  } )
+}
+
+function driverNamesWithRevenueOver() {
+  
+}
+
+function exactMatch() {
+  
+}
+
+function exactMatchToList() {
+  
+}
+
